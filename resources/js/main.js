b2b5b0ff9d276e5ec0d4f13a0335a4f47b620245
Tsolak <tsolakidis.anatolios@gmail.com>
2023-08-23 13:34:21 +0300

    created about page

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,6 +2,10 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const navLinks = document.querySelectorAll(".links");
+const navContainer = document.querySelector(".nav__links");
+
+
 
 navToggle.addEventListener('click' ,() => {
     primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute("aria-expanded" , false) : navToggle.setAttribute('aria-expanded', true);
